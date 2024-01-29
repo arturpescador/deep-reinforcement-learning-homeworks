9e049cd1833ab8c6a0b335f002b37f513216b71e
Caio Jordan <caioj2906@gmail.com>
2024-01-29 18:07:31 +0100

    filled todos

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -129,7 +129,10 @@ class MLPPolicySL(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
         # through it. For example, you can return a torch.FloatTensor. You can also
         # return more flexible objects, such as a
         # `torch.distributions.Distribution` object. It's up to you!
-        raise NotImplementedError
+        #action = torch.distributions.Normal(self.mean_net(observation), torch.exp(self.logstd)).rsample()
+        action = self.mean_net(observation)
+
+        return action
 
     def update(self, observations, actions):
         """
@@ -141,7 +144,16 @@ class MLPPolicySL(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             dict: 'Training Loss': supervised learning loss
         """
         # TODO: update the policy and return the loss
-        loss = TODO
+        observations = ptu.from_numpy(observations)
+        actions = ptu.from_numpy(actions)
+
+        preds = self(observations)
+        loss = (preds - actions).pow(2).mean()
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
